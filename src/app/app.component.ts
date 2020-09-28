0612c756a16a1ae6b470a912ad9bee07e679bbd8
baraskarharshal <baraskarharshal@gmail.com>
2020-09-28 14:11:05 +0530

    7 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+
+  listItems = [
+    {id: 1, name: 'India'},
+    {id: 1, name: 'America'},
+    {id: 1, name: 'China'}
+  ];
+
+  constructor() { }
+  
+  onMovedItem(item: any, list: any[]): void {
+    list.splice(list.indexOf(item), 1);
+  }
+
+  onRemoveAttribute(item: any) {
+      this.listItems.splice(this.listItems.indexOf(item), 1);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { DndListModule } from 'ngx-drag-and-drop-lists';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, DndListModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
